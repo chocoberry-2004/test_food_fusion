9afbbb644c743c428f37eadc1f1dee44322ad8dd
chocoberry-2004 <khanthtalju2004@gmail.com>
2025-12-05 14:00:40 +0630

    The will update db_connection file

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,28 +1,28 @@
 <?php
-// $servername = "localhost";  
-// $username   = "root";      
-// $password   = "root";     
-// $dbname     = "foodfusion_db";
-// $port = "8889";
+$servername = "localhost";  
+$username   = "root";      
+$password   = "root";     
+$dbname     = "foodfusion_db";
+$port = "8889";
 
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname,$port );
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname,$port );
 
-$conn = new mysqli(
-    getenv("DB_HOST"),
-    getenv("DB_USER"),
-    getenv("DB_PASS"),
-    getenv("DB_NAME"),
-    getenv("DB_PORT")
-);
+// $conn = new mysqli(
+//     getenv("DB_HOST"),
+//     getenv("DB_USER"),
+//     getenv("DB_PASS"),
+//     getenv("DB_NAME"),
+//     getenv("DB_PORT")
+// );
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-// Check connection
 // if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 // }
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
